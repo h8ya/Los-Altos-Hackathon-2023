8988e0677401a594f69340940d051a89341b5ef7
h8ya <122123739+h8ya@users.noreply.github.com>
2023-04-08 23:06:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// Read more at http://twil.io/secure
+const externalip = require('external-ip')();
 const twilio = require('twilio');
 require('dotenv').config();
-const client = require("twilio")(process.env.accountSid, process.env.authToken);
-const twiml = new twilio.twiml.VoiceResponse();
-twiml.say('I need help. I need help. I need help, my address is 1234 Bob Street, please send help!');
 
-client.calls.create({
-  twiml: twiml.toString(),
-  to: "+"+"INSERT_PHONE_NUMBER_EXTENSION"+"INSERT_TARGET_PHONE_NUMBER", //without any extension numbers or with it, edit code to accomodate
+// Create a Twilio client with your account credentials
+const client = new twilio(process.env.accountSid, process.env.authToken);
+
+// Get the user's external IP address
+externalip((err, ip) => {
+  if (err) {
+    console.log(err);
+  } else {
+
+    // Format the IP address with spaces and "dot" spelling for each segment
+    const formattedIP = ip.replace(/\./g, ' dot ').replace(/\d/g, '$& ').trim();
+    //const formattedIP = ip.replace(/\./g, ' dot ');
+
+    // Create a TwiML response with the formatted IP address embedded in the message
+    const twiml = new twilio.twiml.VoiceResponse();
+    twiml.say(`I need help. I need help. I need help, my IP address is ${formattedIP}`);
+
+    // Initiate a Twilio call using the TwiML response
+    client.calls.create({
+      twiml: twiml.toString(),
+  to: process.env.number, //without any extension numbers or with it, edit code to accomodate
   from: "INSERT_TWILIO_PHONE_NUMBER",
 })
-  .then(call => console.log(call.sid));
+      .then(call => console.log(call.sid, `\nUser IP: ${ip}`));
+  }
+});
