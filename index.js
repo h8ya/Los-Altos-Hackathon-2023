b090fc08ed3bf64c4997e081015fdd1ac0bd9349
h8ya <122123739+h8ya@users.noreply.github.com>
2023-04-08 18:03:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Read more at http://twil.io/secure
+const twilio = require('twilio');
+require('dotenv').config();
+const client = require("twilio")(process.env.accountSid, process.env.authToken);
+const twiml = new twilio.twiml.VoiceResponse();
+twiml.say('I need help. I need help. I need help, my address is 1234 Bob Street, please send help!');
+
+client.calls.create({
+  twiml: twiml.toString(),
+  to: "+INSERT_PHONE_NUMBER",
+  from: "INSERT_TWILIO_PHONE_NUMBER",
+})
+  .then(call => console.log(call.sid));
